7941b56e67a48d02a34e13b71ba7749ad1c88eda
vvictorious <vvictorious30@gmail.com>
2020-09-18 15:28:44 -0600

    deleted some boiler plate and added nav component

---
+++ react_practice/src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Nav = () => {
+    return (
+        <div className="nav">
+            <h3>Movie List</h3>
+            <p>List of Movies: </p>
+        </div>
+    )
+}
